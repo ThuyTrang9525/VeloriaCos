bd784bddb8e8cfbc15a510e0ecb567d2be454694
MinhHoang1225 <minhhoang121225@gmail.com>
2025-03-28 16:48:13 +0700

    nothing

--- app/Http/Controllers/UserControllers.php
+++ app/Http/Controllers/UserControllers.php
@@ -9,7 +9,53 @@ class UserControllers extends Controller
     function getRegister(){
         return view('Users.register');
     }
+    function postRegister(Request $request){
+        $request->validate([
+            'fullname' => 'required|string|max:255',
+            'email' => 'required|string|max:255',
+            'password' => 'required|string|max:255',
+        ]);
+    }
+    
     function getLogin(){
         return view('Users.login');
     }
 }
+
+
+// public function getLogin(){
+//     return view('page.login');
+// }
+// public function postLogin(Request $request){
+//     $request->validate([
+//         'email' => 'required|email',
+//         'password' => 'required|min:6',
+//     ]);
+//     if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+//         return redirect()->route('trang-chu')->with('success', 'Đăng nhập thành công!');
+//     }
+//     return back()->with('error', 'Email hoặc mật khẩu không đúng!');
+// }
+
+// public function logout()
+// {
+//     Auth::logout();
+//     return redirect()->route('login')->with('success', 'Bạn đã đăng xuất thành công!');
+// }
+// public function getSignup(){
+//     return view('page.signup');
+// }
+
+// public function postSignup(Request $request){
+//     $request->validate([
+//                 'fullname' => 'required|string|max:255',
+//                 'email' => 'required|string|max:255',
+//                 'password' => 'required|string|max:255',
+//             ]);
+//     User::create([
+//         'name' => $request-> fullname,
+//         'email' => $request -> email,
+//         'password' => $request -> password,
+//     ]);
+//     return redirect()->route('login')->with('success', 'Đăng kí thành công');
+// }
